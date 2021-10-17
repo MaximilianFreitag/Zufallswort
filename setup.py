9435c5ebdeff3827248eccd583947e69d800f2e8
Maximilian Freitag <46624616+MaximilianFreitag@users.noreply.github.com>
2021-10-17 18:48:35 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='zufallsworte',
+    version='0.0.3',
+    description='Generates random german words',
+	url='https://github.com/MaximilianFreitag/Zufallswort',
+	author='Maximilian Freitag',
+    author_email='maximilian.freitag@gmx.de',
+    license='MIT',
+    packages=['zufallsworte'],
+    zip_safe=False,
+    python_requires='>=3',
+    install_requires=[]
+    )
