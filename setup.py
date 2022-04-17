68f4dca61890f67fbc70a00142eb50d94e56d834
Maximilian Freitag <46624616+MaximilianFreitag@users.noreply.github.com>
2022-04-17 12:47:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='zufallsworte',
     version='0.0.6',
-    description='Generates random german words',
+    description='Generates random German words from a 300,000 word list',
 	url='https://github.com/MaximilianFreitag/Zufallswort',
 	author='Maximilian Freitag',
     author_email='maximilian.freitag@gmx.de',
